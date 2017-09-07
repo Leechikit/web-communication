3831c665adbacfab9c5ed5b158f2f743666e29ee
李子杰 <leechikit0823@163.com>
2017-09-07 15:26:24 +0800

    postMessage demo

--- src/postMessage_req.js
+++ src/postMessage_req.js
@@ -5,9 +5,10 @@ let buttonEl = document.querySelector('#button');
 let inputEl = document.querySelector('#input');
 let cpLock = false;
 const responseUrl = 'http://127.0.0.1:2333/html/postMessage_res.html';
-
+window.data = 123;
 buttonEl.addEventListener('click', () => {
-    newWindow = window.open(responseUrl, 'new window', 'height=400,width=400,left=200,top=200');
+    newWindow = window.open(responseUrl, 'new window');
+    // , 'height=400,width=400,left=200,top=200'
 });
 
 inputEl.addEventListener('compositionstart', () => {
@@ -22,5 +23,13 @@ inputEl.addEventListener('keyup', (event) => {
     if (!cpLock) {
         let target = event.target;
         newWindow.postMessage(target.value, responseUrl);
+    }
+});
+
+window.addEventListener('message', (event) => {
+    let data = event.data;
+    let origin = event.origin;
+    if (origin = 'http://127.0.0.1:2333' && typeof data == 'string') {
+        console.log(data);
     }
 });
--- src/postMessage_res.js
+++ src/postMessage_res.js
@@ -3,9 +3,11 @@ import './html/postMessage_res.html';
 let messageEl = document.querySelector('#message');
 
 window.addEventListener('message', (event) => {
+    let oldWindow = event.source;
     let data = event.data;
     let origin = event.origin;
     if (origin = 'http://127.0.0.1:2333' && typeof data == 'string') {
         messageEl.innerHTML = data;
+        oldWindow.postMessage('i got it', oldWindow.location.href);
     }
 });
